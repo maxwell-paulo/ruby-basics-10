cad774ac09396f40c78195d28251d8b6df627f3b
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-04-05 09:47:53 -0300

    Final solution

---
+++ lib/vote.rb
@@ -0,0 +1,12 @@
+def allowed_to_vote?(age)
+
+  if age >= 18
+    return true
+  else
+    return false
+  end
+  # TODO: return (not print!) a boolean stating whether `age` is old enough to vote
+  # NOTE: Use an if/else statment
+end
+
+puts allowed_to_vote?(18)
